c6b23aff169c1aaad804932e8d9db162c2f0fb4d
Ваше Имя <poisonsnwy@gmail.com>
2022-01-12 18:37:22 +0200

    2232

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("jrejfjf354324mkmllhqwe");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("jrejfjf354324mkmllhqwe" + scanner.nextInt());
+        System.out.println("jrejfjf354324mkmllhqwe" + scanner.nextInt());
+
     }
 }
